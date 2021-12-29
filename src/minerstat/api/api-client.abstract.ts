28a76a786ee01cecf5a51d3256a2d49fa843f5d2
Andrian Poyda <andrianpoyda@Andrians-MacBook-Pro.local>
2021-12-29 15:31:18 +0200

    useless code removed

--- src/minerstat/api/api-client.abstract.ts
+++ src/minerstat/api/api-client.abstract.ts
@@ -7,17 +7,5 @@ export abstract class MinerstatApiClient {
     this.client = axios.create({
       baseURL: `${process.env.MINERSTAT_BASE_URL}${prefix}`,
     });
-    this.setupErrorInterceptor();
-  }
-
-  private setupErrorInterceptor() {
-    this.client.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      (error) => {
-        console.log(error);
-      },
-    );
   }
 }
